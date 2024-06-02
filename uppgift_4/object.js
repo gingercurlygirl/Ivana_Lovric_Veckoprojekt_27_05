559b5630ab7f5329c4bd6f1df54b4e039c5ed23a
Ivana Lovric <65508754+gingercurlygirl@users.noreply.github.com>
2024-06-02 22:48:31 +0200

    Create object.js

---
+++ uppgift_4/object.js
@@ -0,0 +1,14 @@
+let programmingJokes = [
+    { id: 0, question: "Why don't programmers like nature?", answer: "It has too many bugs." },
+    { id: 1, question: "Why did the CSS developer go to therapy?", answer: "To get rid of his margins." },
+    { id: 2, question: "How do you comfort a JavaScript developer?", answer: " You console them." },
+    { id: 3, question: "Why did the CSS developer leave the restaurant?", answer: "Because it had no class." },
+    { id: 4, question: "Why did the JavaScript developer go missing?", answer: "Because he didn't know when to return." },
+    { id: 5, question: "Why did the HTML tag go to the party?", answer: "Because it wanted to break the line." },
+    { id: 6, question: "Why do JavaScript developers wear glasses?", answer: "Because they don't C#." },
+    { id: 7, question: "Why don't programmers like to use inline styles?", answer: "Because they want to be classy." },
+    { id: 8, question: "Why did the CSS selector break up with the HTML element?", answer: "It found someone more specific." },
+    { id: 9, question: "Why did the CSS developer apply for a job?", answer: "They wanted to get a position." },
+];
+
+export {programmingJokes}
